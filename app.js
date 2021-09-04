a26f1b3f34f34282a64b0dea5ae920bbde9e2d99
mixwebber <89142203+mixwebber@users.noreply.github.com>
2021-09-04 18:25:51 +0400

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-document.getElementsByClassName("button1").onclick = function () {location.replace ("http://google.com")};
-  
+document.getElementById("eventClick").onclick = function () {location.replace ("biz.html")};
+
+
+document.getElementById("but").onclick = function () {window.open ("https://google.com", '_blank')}
