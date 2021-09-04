6ca5b78bf0ebe0385a22adc6c2125cdcaf682b3d
mixwebber <89142203+mixwebber@users.noreply.github.com>
2021-09-04 19:56:44 +0400

    some addition on page

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-document.getElementById("eventClick").onclick = function () {location.replace ("biz.html")};
+let btnOpen = document.querySelectorAll('button');
 
 
-document.getElementById("but").onclick = function () {window.open ("https://google.com", '_blank')}
+btnOpen.forEach(function(el){
+    el.addEventListener('click', () => {
+    window.open ("https://www.google.com", '_blank');
+});
+});
+
+
+
+//* document.getElementById("eventClick").onclick = function () {location.replace ("biz.html")};
+//* document.getElementById("but").onclick = function () {window.open ("http://google.com", '_blank')}
