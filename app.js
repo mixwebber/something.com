71db76e896962747e3b37fa7d3d9bb5886f9fdef
mixwebber <89142203+mixwebber@users.noreply.github.com>
2021-10-04 06:51:24 +0400

    1

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-let btnOpen = document.querySelectorAll('button');
-
-
-btnOpen.forEach(function(el){
-    el.addEventListener('click', () => {
-    window.open ("https://www.google.com", '_blank');
-});
-});
-
-
-
-//* document.getElementById("eventClick").onclick = function () {location.replace ("biz.html")};
-//* document.getElementById("but").onclick = function () {window.open ("http://google.com", '_blank')}
+document.getElementsByClassName("button1").onclick = function () {location.replace ("http://google.com")};
+  
